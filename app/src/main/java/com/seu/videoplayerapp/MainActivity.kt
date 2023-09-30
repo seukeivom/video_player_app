c0de85148f8a910a925c899d52574fadd6c021f9
Israel Keivom <seukeivom@gmail.com>
2023-09-30 13:01:37 +0530

    adding playback functionality from url

--- app/src/main/java/com/seu/videoplayerapp/MainActivity.kt
+++ app/src/main/java/com/seu/videoplayerapp/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.seu.videoplayerapp
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.MediaController
 import android.widget.VideoView
 
 class MainActivity : AppCompatActivity() {
@@ -10,6 +12,18 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val videoView: VideoView = findViewById(R.id.videoview1)
-
+//        videoView.setVideoPath(
+//            "android.resource://"
+//            +packageName+"/"
+//            +R.raw.mountains
+//        )
+        val videoUrl = "https://static.videezy.com/system/resources/previews/000/004/011/original/water.mp4"
+        val uri: Uri = Uri.parse(videoUrl)
+        videoView.setVideoURI(uri)
+        val mediaController = MediaController(this)
+        mediaController.setAnchorView(videoView)
+        mediaController.setMediaPlayer(videoView)
+        videoView.setMediaController(mediaController)
+        videoView.start()
     }
 }
